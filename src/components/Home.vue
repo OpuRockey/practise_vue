0d2159568d8c53026a9326df85b906a6a474f7f8
opurockey <opu.rockey@gmail.com>
2019-02-06 17:33:20 +0600

    Added computed and watch practise

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-        Home sweet Home
+        <input type="number" v-model="inputVal"><br>
+        Home sweet Home<br>
+        Current value         : {{ inputVal }}<br>
+        Computed Double value :{{ doubleInput }}<br>
+        Watched less 5 value  :{{ lessFive }}<br>
+
     </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        data(){
+            return {
+                inputVal : '',
+                lessFive : ''
+            }
+        },
+        computed : {
+            doubleInput(){
+                return this.inputVal * 2 ;
+            }
+        },
+        watch : {
+            inputVal(n,o){
+                console.log('New Value - ' , n);
+                console.log('Old Value - ' , o);
+                this.lessFive = n - 5 ;
+            }
+        }
     }
 </script>
 
